9e835512e4f62728c318153c6c7ad1bdf6c5d07b
Jianne Gabriel <jiannegabriel@gmail.com>
2022-04-11 20:52:18 +0800

    fix error in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-    def custom_javascript_packs
-        begin 
-            "#{javascript_pack_tag params[:controller]}/#{params[:action]}", 'data-turbolinks-track': 'reload'
-        rescue Webpacker::Manifest::MissingEntryError
-        end
-    end
+    # def custom_javascript_packs
+    #     begin 
+    #         "#{javascript_pack_tag params[:controller]}/#{params[:action]}", 'data-turbolinks-track': 'reload'
+    #     rescue Webpacker::Manifest::MissingEntryError
+    #     end
+    # end
 end
